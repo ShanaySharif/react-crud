23aca2e32c3f19838e1cf8144d4143cce1835e40
ShanaySharif <Shanay.uw17@gmail.com>
2023-10-24 15:59:27 -0400

    add routting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 
-import { BrowserRouter, Routes, Rout, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 import './App.css';
+import EditUser from './components/EditUser';
+import CreateUser from './components/CreateUser';
+import ListUser from './components/ListUser';
 
 function App() {
   return (
     <div className="App">
-      <h5> React CRUD operations using PHP and MySQL</h5>
+      <h5> React CRUD operations using PHP API and MySQL</h5>
+
+      <BrowserRouter>
+      <nav> 
+        <ul>
+          <li>
+            <Link to="/"> List Users</Link>
+          </li>
+            <Link to="/"> Create User</Link>
+          <li>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+      <Route index element={<ListUser />} />
+      <Route path="user/create" element= {<CreateUser />} />
+      <Route path="user/:id/edit" element= {<EditUser />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
