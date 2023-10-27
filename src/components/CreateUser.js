2f683a05baed9abc48a26b41726bbc9ac2485dae
ShanaySharif <Shanay.uw17@gmail.com>
2023-10-27 08:54:57 -0400

    finish POST moving on to GET

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,9 +1,12 @@
-import { useState} from "react";
+import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function CreateUser() {
+export default function ListUser() {
+    const navigate = useNavigate();
 
-    const [inputs, setInputs] = useState({})
+    const [inputs, setInputs] = useState([]);
+
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -11,70 +14,51 @@ export default function CreateUser() {
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        
-        axios.post('http://localhost:80/api/user/save', inputs);
-        console.log(inputs);
 
+        axios.post('http://localhost:80/api/user/save', inputs).then(function(response){
+            console.log(response.data);
+            navigate('/');
+        });
+        
     }
     return (
-        <div> 
-            <h1>List Users</h1>
-        <form onSubmit={handleSubmit}> 
-<table cellSpacing = "10">
-    <tbody>
-        <tr>
-            <th>
-            <label> Name:</label>
-
-                
-
-                </th>
-                <td>
-
-                <input type="text" name="name" onChange={handleChange}/>
-
-            </td>
-
-        </tr>
-
-        <tr>
-            <th>
-            <label> Email:</label>
-                
-
-                </th>
-                <td>
-                <input type="text" name="email" onChange={handleChange}/>
-
-
-            </td>
-
-        </tr>
-        <tr>
-            <th>
-            <label> Mobile: </label>
-                </th>
-                <td>
-                <input type= "text" name= "mobile" onChange={handleChange} />
-            </td>
-        </tr>
-        <tr>
-                <td colSpan="2" align ="right">
-                    
-                <button> Save </button>
-
-            </td>
-
-        </tr>
-        
-
-    </tbody>
-
-
-</table>
-
-</form>
-</div>
-
-)
+        <div>
+            <h1>Create user</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing="10">
+                    <tbody>
+                        <tr>
+                            <th>
+                                <label>Name: </label>
+                            </th>
+                            <td>
+                                <input type="text" name="name" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Email: </label>
+                            </th>
+                            <td> 
+                                <input type="text" name="email" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Mobile: </label>
+                            </th>
+                            <td>
+                                <input type="text" name="mobile" onChange={handleChange} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2" align ="right">
+                                <button>Save</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    )
 }
--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,4 +1,26 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+
+
 export default function ListUser() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    function getUsers() {
+        axios.get('http://localhost:80/api/users/').then(function(response) {
+            console.log(response.data);
+            setUsers(response.data);
+        });
+    }
+
+
+
+
+
+
     return (
         <h1>List Users</h1>
 
